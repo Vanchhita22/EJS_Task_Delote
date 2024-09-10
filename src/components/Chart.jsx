281295631bb9e54cbf463fb9cedbf7fba278d18e
Gaurav Waghmare <gwWaghmare@getmybubble.com>
2024-09-10 09:50:56 +0530

    use fetch call

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import "../Asserts/style/chart.css";
@@ -6,19 +6,31 @@ import "../Asserts/style/chart.css";
 // Registering the components needed for the bar chart
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
+const BASE = 'http://localhost:3001'
+
 const Chart = () => {
-  const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'Sales Data',
-        data: [12, 19, 3, 5, 2, 3, 7],
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1,
-      },
-    ],
-  };
+
+  const [data, setData] = useState({
+    labels:[],
+    datasets: [{}]
+  })
+
+  useEffect(() => {
+    fetch( BASE + '/chart').then(res=> res.json()).then(setData)
+  },[])
+
+  // const data = {
+  //   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  //   datasets: [
+  //     {
+  //       label: 'Sales Data',
+  //       data: [12, 19, 3, 5, 2, 3, 7],
+  //       backgroundColor: 'rgba(75, 192, 192, 0.2)',
+  //       borderColor: 'rgba(75, 192, 192, 1)',
+  //       borderWidth: 1,
+  //     },
+  //   ],
+  // };
 
   const options = {
     responsive: true,
@@ -41,7 +53,10 @@ const Chart = () => {
       <h1>Chart</h1>
       <p>This is the Chart section.</p>
       <div style={{ width: '80%', margin: '0 auto' }}>
-        <Bar data={data} options={options} />
+        { data.labels.length ?
+          <Bar data={data} options={options} />
+          : <></>
+        }
       </div>
     </div>
   );
